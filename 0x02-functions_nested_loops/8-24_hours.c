f7fe915c3a2370cc04aebf7bd2ce945d86ca7091
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-03-22 11:20:03 -0700

    comment

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ for (hr = 0; hr <= 23; hr++)
 {
 	for (min = 0; min <= 59; min++)
 	{
-		_putchar(hr / 10 + '0';)
+		_putchar(hr / 10 + '0');
 		_putchar(hr % 10);
 		_putchar(':');
 		_putchar(min / 10 + '0');
