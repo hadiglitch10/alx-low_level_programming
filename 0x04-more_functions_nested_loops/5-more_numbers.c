e02fdf21f04f62597421d61ab46d9523fcf9158d
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-03-24 12:59:36 -0700

    comment

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - print from 0 to 14 ten times
+ *		using only putchar three times
+ *
+ *Return: Always 0 (success)
+*/
+
+void more_numbers(void)
+{
+	int num, row, i;
+
+	for (row = 0; row <= 10; row++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			num = i;
+			if (i > 9)
+			{
+				_putchar(1 + 48);
+				num = i % 10;
+			}
+			_putchar(num + 48)
+		}
+		_putchar('\n')
+	}
+}
