1d4f90d03cd63a1988810ee3d6421961b3cf9083
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-03-23 18:28:18 -0700

    comment

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,7 +15,7 @@ void print_remaining_days(int month, int day, int year)
 	 * leap year when it can be divided
 	 * by 4 evenly or by 100 and 400
 	*/
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 100 == 0))
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month > 2 && day > 60)
 		{
