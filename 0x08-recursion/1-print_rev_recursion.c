c452c41168e413938de9c0803c33eea0c0094a77
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-04-05 07:03:53 -0700

    comment

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _puts_recursion - function puts a rev string;
+ * _print_rev_recursion - function puts a rev string;
  * @s: input string
  * Return: Always 0 (Success)
- */
-void _puts_recursion(char *s)
+*/
+void _print_rev_recursion(char *s)
 {
 	if (*s > '\0')
 	{
-		_puts_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
