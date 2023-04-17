e2b08d07e2949bded57fa988f7ffe38e43bb65a0
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-04-17 14:06:36 -0700

    comment

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef object_MACRO_H
+#define object_MACRO_H
+/**
+ * File: O-object_like_macro
+ *
+ * Desc: haeder file
+ */
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,10 @@
+#ifndef object_MACRO_H
+#define object_MACRO_H
+/**
+ * File: 1-pi
+ *
+ * Desc: header file
+ */
+#define PI 3.14159265359
+
+#endif
