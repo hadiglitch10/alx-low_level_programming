61219943b932902e6f909236db519bb4d41c2c82
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-04-17 18:46:59 -0700

    comment

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Desc: header file
+ */
+struct dog
+{
+	char *name, *owner;
+	float age;
+
+};
+#endif
