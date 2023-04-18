5bf8a1a9e8e612042ec73c59b6fa36a6c3fd6533
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-04-17 19:01:31 -0700

    comment

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,10 +7,17 @@
  * @owner: owner of the dog
  * Desc: header file
  */
-struct dog
+typedef struct dog
 {
 	char *name, *owner;
 	float age;
 
-};
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
 #endif
