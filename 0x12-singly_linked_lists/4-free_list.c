06a0a9d76a127b5909299fd5c73807cb6a5ad992
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-04-27 06:57:46 -0700

    comment

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+/**
+ * free_list - Frees a list_t list.
+ * @head: Pointer to the head of the list.
+ *
+ * Return: void.
+ */
+void free_list(list_t *head)
+{
+	list_t *curr;
+
+	while (head)
+	{
+		curr = head;
+		head = head->next;
+		free(curr->str);
+		free(curr);
+	}
+}
+
