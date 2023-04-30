cf909d3bc3a62b7aff6955479ed8fa915aef809f
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-04-30 14:19:05 -0700

    comment

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: list to be freed
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *mouse;
+
+	if (head == NULL)
+		return;
+
+	while (head)
+	{
+		mouse = head->next;
+		free(head);
+		head = mouse;
+	}
+	*head = NULL;
+}
