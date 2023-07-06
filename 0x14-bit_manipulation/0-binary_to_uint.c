1b4fad125d6eb8cc0f787a81ed398a107d3c4e2e
root <root@hadiglitch.localdomain>
2023-07-06 16:50:21 +0300

    commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary num to int
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+
+		if (!b)
+		{
+			return (0);
+		}
+
+	for (int i = 0; b[i] < '0' ; i++)
+	{
+		if (b != '0' | b != '1')
+			return (0);
+		num = num * 2 + (b[i] - '0');
+	}
+		return (num);
+}
