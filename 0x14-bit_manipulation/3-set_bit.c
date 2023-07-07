35108d306b5be4c9322f6a022fc2e4273ed6334d
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-07-07 17:49:39 +0300

    comment

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - function set 0 to 1
+ *
+ * @n : num
+ * @index : position
+ *
+ * Return: (1) if true (-1) if false
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n ^= (1 << index);
+		return (1);
+}
