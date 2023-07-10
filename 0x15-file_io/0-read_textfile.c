c778fb7d636c5a3e70a0411636d204241faeb2e8
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-07-10 17:35:01 +0300

    comment

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile- read a file and print
+ *
+ * @filename: file name
+ * @letters: num of lettters in file
+ *
+ * Return: return (0) if wrong
+ *          w if true
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fp, r, w;
+	char *buff;
+
+	fp = open(filename, "r");
+
+	if (!fp)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+
+	if (!buff)
+		return (0);
+
+	r = read(fp, buff, letters);
+	w = write(STDOUT_FILENO, buff, r);
+
+	free(buff);
+
+	close(fp);
+
+
+	return (w);
+}
