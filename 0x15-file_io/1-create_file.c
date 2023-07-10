4c24c82ec8a4081e8e7450c6f122ea0d719cafc6
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-07-10 18:15:50 +0300

    comment

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,16 +11,20 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fp, w;
+	int fp, w, n = 0;
 
 	if (!filename)
 		return (-1);
 
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
+	if (!text_content)
+		text_content = "";
 
+	for (; text_content[n]; n++)
+		;
 
-	w = fprintf(fp, "%s", text_content);
+	w = write(fp, text_content, n);
 
 	if (fp == -1 || w == -1)
 		return (-1);
